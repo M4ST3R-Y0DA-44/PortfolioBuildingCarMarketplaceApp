d4d449e72db6b4cc2ff5389ebbdf6044c581a82f
M4ST3R-Y0DA-44 <munyan4@gmail.com>
2021-12-14 00:06:01 -0500

    removal of unfinished files

--- Marketplace.cpp
+++
@@ -1,6 +0,0 @@
-/**
- * \file Marketplace.cpp
- * \author M4ST3R-Y0DA-44
- * \brief This file contains the Marketplace class and all its methods and attributes declaration
- *
- * \date Created on 10-12-2021 */
