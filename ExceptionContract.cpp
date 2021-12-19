003b2f417349a5048aa3d8fc7029832f7642f9b6
M4ST3R-Y0DA-44 <munyan4@gmail.com>
2021-12-18 20:40:16 -0500

    useless code removal in ExceptionContract.cpp

--- ExceptionContract.cpp
+++ ExceptionContract.cpp
@@ -13,13 +13,13 @@ using namespace std;
 
 
 /*Constructor of class ExceptionCont*/
-ExceptionContract::ExceptionContract (std::string p_fichP, unsigned int p_prmLigne,
-                                      std::string p_exprP, std::string p_msgP) :
+ExceptionContract::ExceptionContract (string p_fichP, unsigned int p_prmLigne,
+                                      string p_exprP, string p_msgP) :
 logic_error (p_msgP), m_expression (p_exprP), m_file (p_fichP), m_line (p_prmLigne) { }
 
 
 /*Method to return error string*/
-const std::string
+const string
 ExceptionContract::reqTexteException () const
 {
   // ---  Prépare le message
@@ -34,24 +34,24 @@ ExceptionContract::reqTexteException () const
 
 
 /*Constructor of AssertionException class*/
-AssertionException::AssertionException (std::string p_fichP, unsigned int p_prmLigne,
-                                        std::string p_exprP)
+AssertionException::AssertionException (string p_fichP, unsigned int p_prmLigne,
+                                        string p_exprP)
 : ExceptionContract (p_fichP, p_prmLigne, p_exprP, "ERREUR D'ASSERTION") { }
 
 
 /*Constructor of PreconditionException class*/
-PreconditionException::PreconditionException (std::string p_fichP, unsigned int p_prmLigne,
-                                              std::string p_exprP)
+PreconditionException::PreconditionException (string p_fichP, unsigned int p_prmLigne,
+                                              string p_exprP)
 : ExceptionContract (p_fichP, p_prmLigne, p_exprP, "ERREUR DE PRECONDITION") { }
 
 
 /*Constructor of the PostconditionException class*/
-PostconditionException::PostconditionException (std::string p_fichP, unsigned int p_prmLigne,
-                                                std::string p_exprP)
+PostconditionException::PostconditionException (string p_fichP, unsigned int p_prmLigne,
+                                                string p_exprP)
 : ExceptionContract (p_fichP, p_prmLigne, p_exprP, "ERREUR DE POSTCONDITION") { }
 
 
 /*Constructor of the InvariantException class*/
-InvariantException::InvariantException (std::string p_fichP, unsigned int p_prmLigne,
-                                        std::string p_exprP)
+InvariantException::InvariantException (string p_fichP, unsigned int p_prmLigne,
+                                        string p_exprP)
 : ExceptionContract (p_fichP, p_prmLigne, p_exprP, "ERREUR D'INVARIANT") { }
